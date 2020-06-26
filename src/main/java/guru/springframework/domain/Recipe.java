55341aa1332d60f63de5b41f2e67b6aa867ced86
Glaucio Goncalves <glaucio.c.goncalves@gmail.com>
2020-06-25 23:20:42 -0300

    Added Difficulty to Recipe

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -28,6 +28,9 @@ public class Recipe {
   @Lob
   private Byte[] image;
 
+  @Enumerated(value = EnumType.STRING)
+  private Difficulty difficulty;
+
   public Long getId() {
     return id;
   }
@@ -114,5 +117,13 @@ public class Recipe {
 
   public void setIngredients(Set<Ingredient> ingredients) {
     this.ingredients = ingredients;
+  }
+
+  public Difficulty getDifficulty() {
+    return difficulty;
+  }
+
+  public void setDifficulty(Difficulty difficulty) {
+    this.difficulty = difficulty;
   }
 }
